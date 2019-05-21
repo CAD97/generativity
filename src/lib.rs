fbec551733f719a7606c2e2ec1f419ccb5951350
cad97 <cad97@cad97.com>
2019-05-21 15:07:09 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+#![cfg_attr(not(test), no_std)]
+
+use core::{
+    fmt, marker::PhantomData,
+};
+
+/// A phantomdata-like type taking a single invariant lifetime.
+///
+/// Used to manipulate and store the unique invariant lifetime produce by `Guard`.
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct Id<'id> {
+    phantom: PhantomData<&'id mut &'id fn(&'id ()) -> &'id ()>,
+}
+
+impl<'id> Id<'id> {
+    /// Do not use this function; use the `guard!` macro instead.
+    pub unsafe fn new() -> Self {
+        Id {
+            phantom: PhantomData
+        }
+    }
+}
+
+impl<'id> fmt::Debug for Id<'id> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("#[invariant] 'id").finish()
+    }
+}
+
+impl<'id> From<Guard<'id>> for Id<'id> {
+    fn from(_guard: Guard<'id>) -> Self {
+        Id { phantom: PhantomData }
+    }
+}
+
+/// An invariant lifetime phantomdata that is guaranteed to be unique.
+///
+/// In effect, this means that `'id` is a "generative brand"
+#[derive(Eq, PartialEq)]
+pub struct Guard<'id> {
+    #[allow(unused)]
+    id: Id<'id>,
+}
+
+impl<'id> Guard<'id> {
+    /// Do not use this function; use the `guard!` macro instead.
+    pub unsafe fn new(id: Id<'id>) -> Guard<'id> {
+        Guard { id }
+    }
+}
+
+impl<'id> fmt::Debug for Guard<'id> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("#[unique] 'id").finish()
+    }
+}
+
+/// Create a `Guard` with a unique lifetime.
+///
+/// Multiple invocations will not unify:
+///
+/// ```rust,compile_fail
+/// # use generativity::make_guard;
+/// make_guard!(a);
+/// make_guard!(b);
+/// dbg!(a == b); // ERROR (here == is a static check)
+/// ```
+#[macro_export]
+macro_rules! make_guard {
+    ($name:ident) => {
+        let tag = unsafe { $crate::Id::new() };
+        let _guard;
+        let $name = unsafe { $crate::Guard::new(tag) };
+        { if false {
+            struct D<'id>(&'id $crate::Id<'id>);
+            impl<'id> ::core::ops::Drop for D<'id> {
+                fn drop(&mut self) {}
+            }
+            _guard = D(&tag);
+        } }
+    };
+}
+
+#[test]
+fn dont_error_in_general() {
+    make_guard!(a);
+    make_guard!(b);
+    dbg!(a == a);
+    dbg!(b == b); // OK
+}
